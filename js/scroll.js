0340ab6f945166841f470b0676bed62176297cfc
GustavoCMonteiro <101683431+GustavoCMonteiro@users.noreply.github.com>
2022-04-19 03:06:19 -0300

    scroll animation

---
+++ js/scroll.js
@@ -0,0 +1,24 @@
+const animLeft = document.querySelectorAll(".js-scroll-left");
+const animRight = document.querySelectorAll(".js-scroll-right");
+
+function scrollDown() {
+  const windowHalf = window.innerHeight * 0.6;
+  animLeft.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowHalf;
+    if (sectionTop < 0) {
+      section.classList.add("scroll-left");
+    } else {
+      section.classList.remove("scroll-left");
+    }
+  });
+  animRight.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowHalf;
+    if (sectionTop < 0) {
+      section.classList.add("scroll-right");
+    } else {
+      section.classList.remove("scroll-right");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollDown);
