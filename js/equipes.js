f7dcc819aa7e4c5c62306858071e47420fc91e95
GustavoCMonteiro <101683431+GustavoCMonteiro@users.noreply.github.com>
2022-04-18 23:22:39 -0300

    keyframe

--- js/equipes.js
+++ js/equipes.js
@@ -2,12 +2,14 @@ const equipes = document.querySelectorAll("section");
 
 const botao = document.querySelectorAll(".botao");
 
+equipes[0].classList.add("show");
+
 for(let i = 0; i < botao.length; i++) {
   botao[i].addEventListener("click", function (event){
     event.preventDefault();
     for(let i = 0; i < botao.length; i++) {
-      equipes[i].classList.add("hide");
+      equipes[i].classList.remove("show");
     }
-    equipes[i].classList.remove("hide");
+    equipes[i].classList.add("show");
   });
 }
